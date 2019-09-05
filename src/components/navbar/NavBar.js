89b0661df6022c24d8894707c6c33922c105efed
aio <tom.shegzy@gmail.com>
2019-09-05 19:54:04 +0100

    Pixabay Image Finder Ready for usage

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import AppBar from 'material-ui/AppBar'
+
+const NavBar = ()=> <AppBar title="Pixabay Image Finder" style={{background: 'Navy'}}/>
+
+
+export default  NavBar;
